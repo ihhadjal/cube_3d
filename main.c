0533ba809e4578ee03f3549fbe22e113155b6d7c
Iliane Hasnaoui <ilhasnao@c4r1p2.42nice.fr>
2025-06-18 16:34:12 +0200

    commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ilhasnao <ilhasnao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 19:20:02 by ilhasnao          #+#    #+#             */
-/*   Updated: 2025/06/17 19:49:44 by ilhasnao         ###   ########.fr       */
+/*   Updated: 2025/06/18 16:32:46 by ilhasnao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int	on_keypress(int keycode, t_data *mlx)
 int	main(void)
 {
 	t_data	*mlx;
+	void	*background;
 
 	mlx = malloc(sizeof(t_data));
 	mlx->ptr = mlx_init();
@@ -45,6 +46,7 @@ int	main(void)
 		ft_putstr_fd("Error: Failed to create window\n", 2);
 		mlx_loop_end(mlx->ptr);
 	}
+	background = mlx_xpm_file_to_image()
 	mlx_key_hook(mlx->win, on_keypress, mlx);
 	mlx_hook(mlx->win, 17, 0, free_dem_hoes, mlx);
 	mlx_loop(mlx->ptr);
